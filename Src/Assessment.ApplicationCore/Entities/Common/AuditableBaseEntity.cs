3bb02c84edbeea8c394f8656a3673daa89815b84
Bishnu Rawal <Bishnu.Rawal@verscend.com>
2019-12-24 15:38:32 +0545

    Created base entities

---
+++ Src/Assessment.ApplicationCore/Entities/Common/AuditableBaseEntity.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Assessment.ApplicationCore.Entities
+{
+    public class AuditableBaseEntity: BaseEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+    }
+}
